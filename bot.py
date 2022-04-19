68796aff8faefb8c35502562966f129e2caeb573
FalseHuman <emilkhazioff@yandex.com>
2022-04-19 16:20:16 +0500

    v2

--- bot.py
+++ bot.py
@@ -2,17 +2,18 @@ import telebot
 from telebot import types
 import telethon
 from telethon import TelegramClient, sync
-from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon.tl.functions.channels import JoinChannelRequest
 import sqlite3, time, logging, asyncio, requests
+import config
 
 logging.basicConfig(format='%(asctime)s %(levelname)s - %(message)s',
                     level=logging.INFO, filename='bot.log')
 
-token = 'YOUR_TOKEN'
+token = config.token
 
 bot = telebot.TeleBot(str(token))
 
-owner = 'YOUR_ID_TELEGRAM'
+owner = config.owner
 
 
 @bot.message_handler(commands=['start'])
@@ -20,7 +21,14 @@ def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
     btn = types.KeyboardButton('Зарегистрироватьcя')
     btn2 = types.KeyboardButton('Посмотреть список пользователей')
-    btn3 = types.KeyboardButton('Посмотреть список групп')
+    btn3 = types.KeyboardButton('Посмотреть список каналов')
+    if owner == message.chat.id:
+        markup.add(btn)
+        markup.add(btn2)
+        markup.add(btn3)
+    else:
+        markup.add(btn)
+        markup.add(btn3)
     connect = sqlite3.connect('group.db')
     cursor = connect.cursor()
     cursor.execute("""CREATE TABLE IF NOT EXISTS user_manager(
@@ -33,29 +41,16 @@ def start(message):
     )""")
     connect.commit()
     user_id = message.chat.id
-    cursor.execute(f"SELECT id FROM user_manager WHERE id = {user_id}")
+    cursor.execute(f"SELECT id FROM user_manager WHERE id ={user_id}")
     data = cursor.fetchone()
     if data is None:
         cursor.execute(f"INSERT INTO user_manager VALUES(?, ?, ?, ?, ?, ?);",
                        (user_id, user_id, message.from_user.first_name, user_id, user_id, user_id))
         connect.commit()
-        if message.chat.id == owner:
-            markup.add(btn2)
-            markup.add(btn3)
-            send_message = f"<b>Привет {message.from_user.first_name}!</b>\ Для того чтобы воспользоваться ботом нажми /instruction"
-            bot.send_message(message.chat.id, send_message,
-                             parse_mode='html', reply_markup=markup)
-        else:
-            markup.add(btn)
-            send_message = f"<b>Привет {message.from_user.first_name}!</b>\nДля того чтобы воспользоваться ботом нажми /instruction"
-            bot.send_message(message.chat.id, send_message,
+        send_message = f"<b>Привет {message.from_user.first_name}!</b> Для того чтобы воспользоваться ботом нажми /instruction"
+        bot.send_message(message.chat.id, send_message,
                              parse_mode='html', reply_markup=markup)
     else:
-        if message.chat.id == owner:
-            markup.add(btn3)
-            markup.add(btn2)
-        else:
-            markup.add(btn)
         bot.send_message(message.chat.id, f'Мы Вас помним:) Для того чтобы воспользоваться ботом нажми /instruction',
                          parse_mode='html', reply_markup=markup)
 
@@ -69,35 +64,20 @@ def get_id(message):
 
 @bot.message_handler(commands=['instruction'])
 def messages(message):
-
-    if message.chat.id == owner:
-        user_owner = """<b>Инструкция:</b>\nДля того чтобы посмотреть список пользователей нажми Посмотреть список пользователей\nДля того чтобы посмотреть список групп - Посмотреть список групп\nДля того чтобы добавить группу нажми - /add_group\nЧтобы добавить пользователей в группы нажми -/group_user_add"""
-        bot.send_message(message.chat.id, user_owner, parse_mode='html')
-    else:
-        no_owner = """<b>Инструкция:</b>\nДля того чтобы зарегистрироваться нажми кнопку Зарегистрироваться, предварительно нужно зарегистрировать приложение тут - Заходим в пункт "API". Ищем "Telegram API" и заходим в "Creating an application" (https://my.telegram.org/apps).\nДля того чтобы подключиться /connect (P.S. после того как нажал кнопку Зарегистрироваться и ввел данные)"""
-        bot.send_message(message.chat.id, no_owner, parse_mode='html')
+        instruction = """<b>Инструкция:</b>\nДля того чтобы зарегистрироваться нажми кнопку Зарегистрироваться, предварительно нужно зарегистрировать приложение тут - Заходим в пункт "API". Ищем "Telegram API" и заходим в "Creating an application" (https://my.telegram.org/apps).\nДля того чтобы подключиться /connect (P.S. после того как нажал кнопку Зарегистрироваться и ввел данные)\nДля того чтобы добавить группу введи её ссылку, например https://t.me/durov \nДля того чтобы добавиться в группы /group_user_add"""
+        bot.send_message(message.chat.id, instruction, parse_mode='html')
 
 
-@bot.message_handler(commands=['add_group'])
-def messages(message):
-    connect = sqlite3.connect('group.db')
-    cursor = connect.cursor()
-    final_message = 'Введите данные через запятую в таком формате: Группа: название группы, ссылка на группу'
-    cursor.execute("""CREATE TABLE IF NOT EXISTS group_link(
-        id INTEGER PRIMARY KEY,
-        namegroup TEXT,
-        linkgroup TEXT
-    )""")
-    connect.commit()
-    bot.send_message(message.chat.id, final_message, parse_mode='html')
 
 
 @bot.message_handler(commands=['group_user_add'])
 def messages(message):
     connect = sqlite3.connect('group.db')
     cursor = connect.cursor()
-    cursor.execute(f"SELECT id, api_id, api_hash FROM user_manager")
+    user_id = message.chat.id
+    cursor.execute(f"SELECT id, api_id, api_hash FROM user_manager WHERE id ={user_id}")
     data = cursor.fetchall()
+    print(data)
     for user in data:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
@@ -105,28 +85,30 @@ def messages(message):
             client = TelegramClient(
                 f'{user[0]}', user[1], user[2], loop=loop)
             client.connect()
-            cursor.execute(f"SELECT linkgroup FROM group_link")
+            cursor.execute(f"SELECT linkgroup FROM group_link WHERE chat_id={user_id}")
             group = cursor.fetchall()
             for link in group:
-                if 'joinchat' in link:
-                    link = link[0].replace("https://t.me/joinchat/", "")
-                else:
-                    link = link[0].replace("https://t.me/+", "")
+                links = link[0].replace("https://t.me/", "")
+                print()
                 try:
                     if client.is_user_authorized():
-                        client(ImportChatInviteRequest(link))
+                        print(f'{user} join')
+                        client(JoinChannelRequest(links))
                         time.sleep(3)
                 except Exception as e:
-                    pass
+                    print(e)
+            url = "https://api.telegram.org/bot" + token
+            chat_id=f'{user[0]}'
+            method = url + "/sendMessage"
+            text = 'Вы были добавлены в канал/-ы'
+            r = requests.post(method, data={"chat_id": chat_id, "text": text})
             client.disconnect()
         except Exception as e:
             url = "https://api.telegram.org/bot" + token
             chat_id=f'{user[0]}'
             method = url + "/sendMessage"
-            text = 'Была произведена попытка добавления Вас в список групп, но вы не авторизованы в приложении. Пожалуйста авторизуйтесь и попросите добавить Вас ещё раз!'
+            text = 'Была произведена попытка добавления Вас в список групп, но вы не авторизованы в приложении. Пожалуйста авторизуйтесь и нажмите команду /group_user_add!'
             r = requests.post(method, data={"chat_id": chat_id, "text": text})
-    final_message = 'Был произведен процесс добавления пользователей в группы'
-    bot.send_message(message.chat.id, final_message, parse_mode='html')
 
 
 @bot.message_handler(commands=['connect'])
@@ -166,7 +148,7 @@ def mess(message):
     user_id = message.chat.id
     get_message_bot = message.text
     cursor.execute(
-        f"SELECT id, api_id, api_hash, phone, phone_code_hash FROM user_manager WHERE id = {message.chat.id}")
+        f"SELECT id, api_id, api_hash, phone, phone_code_hash FROM user_manager WHERE id = {user_id}")
     data = cursor.fetchone()
     if get_message_bot == 'Посмотреть список пользователей' and message.chat.id == owner:
         cursor.execute(f"SELECT * FROM user_manager")
@@ -175,25 +157,30 @@ def mess(message):
         for user in data:
             message_all_user += f'Менеджер: {user[2]} - Удалить: /delete_{str(user[0])}\n------\n'
         bot.send_message(message.chat.id, message_all_user, parse_mode='html')
-    elif get_message_bot == 'Посмотреть список групп' and message.chat.id == owner:
+    elif get_message_bot == 'Посмотреть список каналов':
         cursor.execute(f"SELECT * FROM group_link")
         data = cursor.fetchall()
-        message_all_user = 'Список групп: \n'
+        message_all_user = 'Список каналов: \n'
         for user in data:
-            message_all_user += f'Название группы: {user[1]}\nСсылка: {user[2]}\nУдалить: /delete_group_{str(user[0])}\n------\n'
+            if user[1] == message.chat.id:
+                message_all_user += f'Ссылка: {user[2]}\nУдалить: /delete_group_{str(user[0])}\n------\n'
         bot.send_message(message.chat.id, message_all_user, parse_mode='html')
     elif '/delete_' in get_message_bot and 'group' not in get_message_bot and message.chat.id == owner:
         user_id = get_message_bot.replace("/delete_", "")
-        cursor.execute(f"DELETE FROM user_manager WHERE id = {int(user_id)}")
+        cursor.execute(f"DELETE FROM user_manager WHERE id = {user_id}")
+        cursor.execute(f"DELETE FROM group_link WHERE chat_id = {user_id}")
         connect.commit()
         bot.send_message(
             message.chat.id, f"Пользователь с  id={user_id} удален!")
-    elif '/delete_group_' in get_message_bot and message.chat.id == owner:
-        user_id = get_message_bot.replace("/delete_group_", "")
-        cursor.execute(f"DELETE FROM group_link WHERE id = {int(user_id)}")
-        connect.commit()
-        bot.send_message(message.chat.id, f"Группа с  id={user_id} удален!")
-    elif ',' not in get_message_bot and "Код" not in get_message_bot and 'Группа' not in get_message_bot:
+    elif '/delete_group_' in get_message_bot:
+        group_id = get_message_bot.replace("/delete_group_", "")
+        try:
+            cursor.execute(f"DELETE FROM group_link WHERE id = ? AND chat_id=?", data)
+            connect.commit()
+            bot.send_message(message.chat.id, f"Группа с  id={group_id} удален!")
+        except:
+            bot.send_message(message.chat.id, "Вы пытаетесь удалить группу которой не существует!")
+    elif ',' not in get_message_bot and "Код" not in get_message_bot and 'https://t.me/' not in get_message_bot:
         bot.send_message(
             message.chat.id, f'Введите данные через запятую в таком формате: api_id, api_hash, номер телефона')
     elif ',' in get_message_bot and "Код" not in get_message_bot and 'Группа' not in get_message_bot:
@@ -223,13 +210,22 @@ def mess(message):
         else:
             bot.send_message(message.chat.id, f'Вы уже подключены')
         client.disconnect()
-    elif 'Группа' in get_message_bot and '/add_group' not in get_message_bot:
-        get_message_bot = get_message_bot.replace('Группа:', '')
-        get_message_bot = get_message_bot.split(',')
-        data = (get_message_bot[0].strip(), get_message_bot[1].strip())
-        cursor.execute(f"INSERT INTO group_link VALUES(NULL, ?, ?);", data)
+    elif 'https://t.me/' in get_message_bot:
+        connect = sqlite3.connect('group.db')
+        cursor = connect.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS group_link(
+            id INTEGER PRIMARY KEY,
+            chat_id INTEGER,
+            linkgroup TEXT
+        )""")
         connect.commit()
-        bot.send_message(message.chat.id, "Данные добавил!")
+        get_message_bot = get_message_bot.split('\n')
+        print(get_message_bot)
+        for link in get_message_bot:
+            data = (message.chat.id, link)
+            cursor.execute(f"INSERT INTO group_link VALUES(NULL, ?, ?);", data)
+            connect.commit()
+        bot.send_message(message.chat.id, "Канал/-ы добавлены!")
 
 
 while True:
